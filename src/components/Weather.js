b55e695d716b003f03c1876c5df83584fdd55745
rafał-Komputer\rafał <rafallukasik123@wp.pl>
2019-03-26 19:16:48 +0100

    final no style css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
-const api_key='10d35fcce7c02936d78262262d8ebda4';
 
 class Weather extends Component {
 constructor(props){
@@ -27,19 +25,38 @@ constructor(props){
 
 
         const icon=this.props.object.list[0].weather[0].icon;
-        const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+        const iconurlnow = "http://openweathermap.org/img/w/" + icon + ".png";
         const time=this.props.object.list[0].dt_txt;
         const location=this.props.object.city.name;
         const main=this.props.object.list[0].weather[0].main;
         const description=this.props.object.list[0].weather[0].description;
-console.log(time);
+        let temperature=this.props.object.list[0].main.temp/272.15;
+        temperature=temperature.toFixed(2);
+
+        const icon2=this.props.object.list[1].weather[0].icon;
+        const iconurlnow2 = "http://openweathermap.org/img/w/" + icon2 + ".png";
+        const time2=this.props.object.list[1].dt_txt;
+        
+        const main2=this.props.object.list[1].weather[0].main;
+        const description2=this.props.object.list[1].weather[0].description;
+        let temperature2=this.props.object.list[1].main.temp/272.15;
+        temperature2=temperature2.toFixed(2);
+
 
       return (
         <section>
-          <p> <span>Location:</span>  <span>{location}</span>  </p>
-          <p> <span>Now Weather last update <span>{time}</span>:</span>  <span>{main}</span>  : <span>{description}</span> </p>
-          <div><img src={iconurl}/>     </div>
+                <div>
+                <p> <span>Location:</span>  <span>{location}</span>  </p>
+                <p> <span>Weather last update <span>{time}</span>:</span>  <span>{main}</span>  : <span>{description}</span> </p>
+                <div><img src={iconurlnow} alt="icon weather"/>  <span>Current temperature : {temperature} &#8451; </span>    </div>
+                </div>
 
+                  <div>
+                    <h2>Next hours</h2>
+                    <p> <span>Weather next update <span>{time2}</span>:</span>  <span>{main2}</span>  : <span>{description2}</span> </p>
+                <div><img src={iconurlnow2} alt="icon weather"/>  <span>Predicted temperature : {temperature2} &#8451; </span>    </div>
+
+                  </div>
         </section>
       );
     }
