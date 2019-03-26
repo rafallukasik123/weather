9a001fa0ea95da53cbae432296b565e2317eb3d9
rafał-Komputer\rafał <rafallukasik123@wp.pl>
2019-03-26 17:42:46 +0100

    displaying items

--- src/App.js
+++ src/App.js
@@ -9,27 +9,61 @@ class App extends Component {
 
   constructor(){
     super();
+this.state={
+  wheather_array : [],
+  name_city : '',
+  wheather_object : '',
 
+}
   }
 
   
 
   componentDidMount(){
     
-    axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=Lublin&APPID=${api_key}`)
-    .then(res => {
-      const persons = res.data.list[0].weather[0].main;
-      console.log(persons);
-    })
+    
+    
   }
 
+handleInputChange = (e) => {
+  e.preventDefault();
+  this.setState({
+    name_city : e.target.value,
+  })
+
+}
+
+handleSubmit= (e) =>{
+  e.preventDefault();
+  const name=this.state.name_city;
+  console.log(name);
+  
+  axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${name}&APPID=${api_key}`)
+    .then(res => {
+      //const wheather_object = res.data.list[0].weather[0].main;
+      const wheather_object = res.data;
+      console.log(wheather_object);
+      this.setState({
+        wheather_array : wheather_object.list,
+        wheather_object
+      })
+
+    }).catch(error=> {
+      
+      console.log(error);
+    })
+  
+}
  
   render() {
+
+    
+
     return (
       <main className="App">
         <Title/>
-        <Form/>
-        <Weather/>
+        <Form  changeinput={this.handleInputChange} input_submit={this.handleSubmit }   />
+        <Weather object={this.state.wheather_object} />
 
 
       </main>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
 
-
+const citylist = ['Lublin','Biłgoraj','Świdnik'];
 
 class Form extends Component {
+constructor(props){
+  super(props);
+
+  this.state = {
+    query: '',
+    filteredCity: citylist
+  }
+
+  
+}
+handleInputChange = (e) => {
+  this.setState({
+    query: e.target.value
+  })
+}
+filterCity(e) {
+  const text = e.currentTarget.value;
+  const filteredCity = this.getFilteredCityForText(text);
+  this.setState({
+    filteredCity 
+  })
+}
+
+getFilteredCityForText(name) {
+  return citylist.filter(id => id.toLowerCase().includes(name.toLowerCase()))
+}
+
+
+
+Suggestions = () => {
+  const options = this.state.filteredCity.map(r => (
+    <li key={r.toString()}>
+      {r}
+    </li>
+  ))
+  return <ul>{options}</ul>
+}
+
   render() {
     return (
-      <div className="App">
-       Form
-      </div>
+     
+       <form onSubmit={this.props.input_submit}>
+         <input
+         placeholder="Search for..."
+       onInput={this.filterCity.bind(this)}
+        onChange={this.props.changeinput}
+
+        
+       />
+       <input type="submit" value="Get Weather"/>
+       <this.Suggestions/>
+
+       </form>
+      
     );
   }
 }
