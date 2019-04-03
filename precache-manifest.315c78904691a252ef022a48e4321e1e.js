47fd6e5029e25f00de3f4d78eecc119dc42f186a
rafał-Komputer\rafał <rafallukasik123@wp.pl>
2019-04-03 16:40:41 +0200

    Updates

---
+++ precache-manifest.315c78904691a252ef022a48e4321e1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f506150c28e321ae23d3d817695f9fb",
+    "url": "/weather/static/media/background.9f506150.jpg"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "2ac9a5c8ccdd14e7e0c3",
+    "url": "/weather/static/js/main.0ea3bcf5.chunk.js"
+  },
+  {
+    "revision": "a513c0304659b794d608",
+    "url": "/weather/static/js/2.91cfaca1.chunk.js"
+  },
+  {
+    "revision": "2ac9a5c8ccdd14e7e0c3",
+    "url": "/weather/static/css/main.972b0e2a.chunk.css"
+  },
+  {
+    "revision": "9d16da2608026e98aa77a3579e259bc6",
+    "url": "/weather/index.html"
+  }
+];
